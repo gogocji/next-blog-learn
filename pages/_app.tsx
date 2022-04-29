16edb1270799eb6aeb8281bf632cf26a7763a34f
gogocj <1329110070@qq.com>
2022-04-29 17:43:40 +0800

    feat: cookie保存内容注入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,41 @@ import 'styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from 'components/layout'
 import { StoreProvider } from 'store/index'
+import { NextPage } from 'next'
 
-function MyApp({ Component, pageProps }: AppProps) {
+interface IProps {
+  initialValue: Record<any, any>
+  Component: NextPage
+  pageProps: any
+}
+
+function MyApp({ initialValue, Component, pageProps }: IProps) {
   return (
-    <StoreProvider initialValue={{ user: {} }}>
+    <StoreProvider initialValue={initialValue}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </StoreProvider>
   )
+}
+
+// TODO: 最好还是自己定义一下类型，不要全部都用any来对类型进行声明了
+MyApp.getInitialProps = async ({ ctx }: { ctx: any }) => {
+  console.log('+++++++')
+  console.log(ctx?.req.cookies)
+  const { userId, nickname, avatar } = ctx?.req.cookies || {}
+
+  return {
+    initialValue: {
+      user: {
+        userInfo: {
+          userId,
+          nickname,
+          avatar
+        }
+      }
+    }
+  }
 }
 
 export default MyApp
--- store/rootStore.ts
+++ store/rootStore.ts
@@ -7,7 +7,7 @@ export interface IStore {
 export default function createStore(initialValue: any): () => IStore {
   return () => {
     return {
-      user: { ...userStore(), ...initialValue?.user }
+      user: { ...userStore(), ...initialValue?.user } // 这里的...initialValue会对userStore的一些默认key进行覆盖更新
     };
   };
 }
