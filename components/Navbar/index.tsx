4dad59da95aa8c3e6c0dd3d1e24ba733b5fbc467
gogocj <1329110070@qq.com>
2022-04-29 17:11:16 +0800

    feat: 用户登录流程完成

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,11 +3,15 @@ import { useRouter } from 'next/router';
 import styles from './index.module.scss';
 import { navs } from './config';
 import type { NextPage } from 'next'
-import { Button } from 'antd'
+import { Button, Avatar, Dropdown, Menu } from 'antd'
 import { useState } from 'react'
 import Login from 'components/Login/index'
+import { useStore } from 'store/index';
+import { LoginOutlined, HomeOutlined } from '@ant-design/icons'
 
 const Navbar: NextPage = () => {
+  const store = useStore()
+  const { userId, avatar } = store.user.userInfo
   const { pathname } = useRouter();
   const [ isShowLogin, setIsShowLogin ] = useState(false);
 
@@ -21,6 +25,21 @@ const Navbar: NextPage = () => {
 
   const handleClose = () => {
     setIsShowLogin(false);
+  }
+
+  const renderDropDownMenu = () => {
+    return (
+      <Menu>
+        <Menu.Item>
+          <HomeOutlined/>
+          &nbsp; 个人主页
+        </Menu.Item>
+        <Menu.Item>
+          <LoginOutlined/>
+          &nbsp; 退出系统
+        </Menu.Item>
+      </Menu>
+    )
   }
   
   return (
@@ -37,7 +56,17 @@ const Navbar: NextPage = () => {
       </section>
       <section className={styles.operationArea}>
         <Button onClick={handleGotoEditorPage}>写文章</Button>
-        <Button type="primary" onClick={handleLogin}>登录</Button>
+          {
+            userId ? (
+              <>
+                  <Dropdown overlay={renderDropDownMenu()} placement="bottomLeft">
+                    <Avatar src={avatar} size={32} />
+                  </Dropdown>
+              </>
+            ) : (
+              <Button type="primary" onClick={handleLogin}>登录</Button>
+            )
+          }
       </section>
       <Login isShow={isShowLogin} onClose={handleClose}/>
     </div>
