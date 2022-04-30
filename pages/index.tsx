c4b49286d066033effe29f9c639a87cd03865dde
gogocj <1329110070@qq.com>
2022-04-30 20:58:44 +0800

    feat: 首页的tags分类切换

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,86 @@
 import { prepareConnection } from "db/index"
-import { Article } from "db/entity"
+import { Article, Tag } from "db/entity"
 import { Divider } from 'antd'
-import ListItem from 'components/ListItem'
 import { IArticle } from 'pages/api/index'
+import styles from './index.module.scss';
+import dynamic from 'next/dynamic';
+import request from 'service/fetch';
+import { useState, useEffect } from 'react';
+import classnames from 'classnames';
+
+const DynamicComponent = dynamic(() => import('components/ListItem'));
+
+interface ITag {
+  id: number;
+  title: string;
+}
 
 interface IProps {
-  articles: IArticle[]
+  articles: IArticle[],
+  tags: ITag[];
 }
 
 export async function getServerSideProps() {
   const db = await prepareConnection()
   const articles = await db.getRepository(Article).find({
-    relations: ['user']
+    relations: ['user', 'tags']
   })
+  const tags = await db.getRepository(Tag).find({
+    relations: ['users'],
+  });
 
   return {
     props: {
-      articles: JSON.parse(JSON.stringify(articles)) || []
+      articles: JSON.parse(JSON.stringify(articles)) || [],
+      tags: JSON.parse(JSON.stringify(tags)) || [],
     }
   }
 }
 
 const Home = (props: IProps) => {
-  const { articles } = props
-  console.log('获取到的文章数据', articles)
+  const { articles, tags } = props;
+  const [selectTag, setSelectTag] = useState(0);
+  const [showAricles, setShowAricles] = useState([...articles]);
+
+  const handleSelectTag = (event: any) => {
+    const { tagid } = event?.target?.dataset || {};
+    setSelectTag(Number(tagid));
+  };
+
+  useEffect(() => {
+    selectTag &&
+      request.get(`/api/article/get?tag_id=${selectTag}`).then((res: any) => {
+        if (res?.code === 0) {
+          setShowAricles(res?.data);
+        }
+      });
+  }, [selectTag]);
+
   return (
     <div>
-      {
-        articles?.map((article) => (
+      <div className={styles.tags} onClick={handleSelectTag}>
+        {tags?.map((tag) => (
+          <div
+            key={tag?.id}
+            data-tagid={tag?.id}
+            className={classnames(
+              styles.tag,
+              selectTag === tag?.id ? styles['active'] : ''
+            )}
+          >
+            {tag?.title}
+          </div>
+        ))}
+      </div>
+      <div className="content-layout">
+        {showAricles?.map((article) => (
           <>
-            <ListItem article={article} />
-            <Divider/>
+            {/* <ListItem article={article} /> */}
+            <DynamicComponent article={article} />
+            <Divider />
           </>
-        ))
-      }
+        ))}
+      </div>
     </div>
   )
 }
