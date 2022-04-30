3544a2bdd8ab6138944dc0e99de96e8e1c5bd664
gogocj <1329110070@qq.com>
2022-04-30 20:19:53 +0800

    feat: 文章tags标签页面展示和关注

--- db/entity/article.ts
+++ db/entity/article.ts
@@ -1,6 +1,7 @@
-import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, OneToMany } from 'typeorm';
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, OneToMany, ManyToMany } from 'typeorm';
 import { Comment } from './comment'
 import { User } from './user'
+import { Tag } from './tag'
 
 @Entity({name: 'articles'})
 export class Article extends BaseEntity {
@@ -31,4 +32,9 @@ export class Article extends BaseEntity {
 
   @OneToMany(() => Comment, (comment) => comment.article)
   comments!: Comment[]
+
+  @ManyToMany(() => Tag, (tag) => tag.articles, {
+    cascade: true
+  })
+  tags!: Tag[]
 }
